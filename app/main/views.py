9b40516f8ec29c46b678d2bc034a0ace07bf7a89
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-06-13 17:42:24 +0300

    create a profile.html to edit the route for the user and add a route for the user data

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,9 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from flask_login import login_required
 from . forms import UpdateProfile, UploadPitch, CommentsForm
 from .. import  db
+from ..models import User, Pitch, Category,Votes,Comment
 
 # Views
 @main.route('/')
@@ -19,3 +20,13 @@ def pitch():
     pitches = Pitch.query.filter_by().first()
 
     return render_template('pitch.html', pitches = pitches)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
