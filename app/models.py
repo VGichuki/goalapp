937fdab85cc9ac13d7d5150b1799abf659b9f131
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-06-13 11:43:40 +0300

    create a password secure to the class User

--- app/models.py
+++ app/models.py
@@ -1,17 +1,31 @@
 from . import db
 from datetime import datetime,timezone
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__='users'
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(255))
-    pass_secure = db.Column(db.String(255))
     image_path = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+   
 
 
     def __repr__(self):
-       return f'User {self.username}'
+       return f'User:{self.username}'
 
 
 class Pitch(db.Model):
@@ -26,4 +40,32 @@ class Pitch(db.Model):
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
 
     def __repr__(self):
-      return f'User {self.pitch}'
+      return f'Pitch:{self.pitch}'
+
+class Category(db.Model):
+    __tablename__ = 'category'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(255))
+    pitch = db.relationship("Pitch",backref='category',lazy='dynamic')
+
+    def __repr__(self):
+      return f'Category:{self.name}'
+
+class Comment(db.Model):
+    __tablename__='comments'
+    id = db.Column(db.Integer,primary_key=True)
+    comment = db.Column(db.String(255))
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    pitch_id = db.Column(db.Integer,db.ForeignKey("pitch.id"))
+    def __repr__(self):
+      return f'Comment:{self.comment}'
+
+class Votes(db.Model):
+    __tablename__='votes'
+    id = db.Column(db.Integer,primary_key=True)
+    vote = db.Column(db.Integer)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    pitch_id = db.Column(db.Integer,db.ForeignKey("pitch.id"))
+    def __repr__(self):
+      return f'Votes:{self.vote}'
