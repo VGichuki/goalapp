cabc9d800b5e82e8670c5f84114a777998f902e3
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-06-14 08:04:35 +0300

    create a method for the user to upload a photo in the website

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from ..models import User
 from .forms import RegistrationForm, LoginForm
 from ..import db
 from flask_login import login_user,logout_user,login_required
+from ..email import mail_message
 
 
 @auth.route('/register',methods = ["GET","POST"])
@@ -13,6 +14,7 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+        mail_message("Welcome to 60seconds of truth","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
