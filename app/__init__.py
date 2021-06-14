341d06bdfb7b2b94d6fbd9ede94a6aa0736c7b8f
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-06-14 08:03:47 +0300

    create a mail server to send emails to a new user

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,17 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_manager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos', IMAGES)
+mail = Mail()
 
 
 def create_app(config_name):
@@ -30,6 +35,12 @@ def create_app(config_name):
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    #configure UploadSet
+    configure_uploads(app,photos)
+
+    #Mail configuration
+    mail.init_app(app)
 
 
     return app 
--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 export SECRET_KEY = 'ac78654356g'
 
+export MAIL_USERNAME='cyclistspace@gmail.com'
+export MAIL_PASSWORD='groupproject'
+
 python3.8 manage.py server
